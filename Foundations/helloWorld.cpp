09934ec9ecb38c804c8944739cfb2bce6de105c8
Chris Goode <hello@chrisgoode.me>
2017-07-25 13:22:25 -0400

    More playing

--- Foundations/helloWorld.cpp
+++ Foundations/helloWorld.cpp
@@ -1,20 +1,41 @@
 // my first program in c++
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+string helloWorld()
+{
+	return "Hello World! \r\n";
+}
 
 int main() 
 {
 	int choice;
 
-	std::cout << "Hello World! This is my first C++ thing." << std::endl;
-	std::cout << "What would you like to do? \n";
-	std::cout << "1. Hello World \n";
-	std::cout << "2. Variable Use \n";
-	std::cout << "3. not yet \n";
-	std::cin >> choice;
+	cout << "Hello World! This is my first C++ thing. \r\n" << endl;
 
-	std::cout << "You chose " << choice << std::endl;
 
-	system("pause");
+	do {
+		cout << "What would you like to do? \n";
+		cout << "1. Hello World \n";
+		cout << "2. Variable Use \n";
+		cout << "Press 0 to exit.\n";
+		cin >> choice;
+		switch (choice)
+		{
+		case 0: 
+			break;
+		case 1:
+			cout << helloWorld() << endl;
+			break;
+
+		}
+
+	} while (choice != 0);
 
 	return 0;
+
 }
+
+
